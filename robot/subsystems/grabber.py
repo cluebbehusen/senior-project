2a926ed2b211171860427f3871ecc2a42e14957c
Connor Luebbehusen <connorluebbehusen@gmail.com>
2022-04-11 11:33:58 -0500

    finish draft of grabber subsystem class

--- robot/subsystems/grabber.py
+++ robot/subsystems/grabber.py
@@ -9,8 +9,15 @@ from robot.hardware.linear_actuator import LinearActuator
 class Grabber:
     """Class for cleanly controlling grabber subsystem"""
 
+    grab_initial_delay: float = 0.0
+    grab_actuator_min_pwm: int = 0
+    grab_actuator_max_pwm: int = 0
+    grab_motor_pwm: int = 0
+    grab_extension_delay: float = 0.0
+    grab_end_delay: float = 0.0
+    retract_delay: float = 0.0
     cup_distance: int = 0
-    cup_extension_time: float = 0.0
+    cup_extension_delay: float = 0.0
     dispense_pwm: int = 0
     dispense_time: float = 0.0
 
@@ -34,16 +41,27 @@ class Grabber:
 
     def grab(self):
         """Extend grabbing mechanism and grab beads"""
-        pass
+        self.motor.set_direction_forward()
+        self.motor.set_motor_pwm(self.grab_motor_pwm)
+        self.actuator.set_extension_pwm(self.grab_actuator_min_pwm)
+        time.sleep(self.grab_initial_delay)
+        for pwm in range(
+                self.grab_actuator_min_pwm,
+                self.grab_actuator_max_pwm):
+            self.actuator.set_extension_pwm(pwm)
+            time.sleep(self.grab_extension_delay)
+        time.sleep(self.grab_end_delay)
+        self.motor.set_motor_pwm(0)
 
     def retract(self) -> None:
         """Retract grabbing mechanism"""
         self.actuator.set_extension_minimum()
+        time.sleep(self.retract_delay)
 
     def extend_to_cup(self) -> None:
         """Extend grabbing mechanism to hover over cup"""
         self.actuator.set_extension_percent(self.cup_distance)
-        time.sleep(self.cup_extension_time)
+        time.sleep(self.cup_extension_delay)
 
     def dispense_beads(self) -> None:
         """Rotate grabbing mechanism to drop beads"""
