9127e97e2edd90f4d85f70097c3f13eaab54a415
Connor Luebbehusen <connorluebbehusen@gmail.com>
2022-04-11 11:02:29 -0500

    fixed some type issues wwith grabber class

--- robot/hardware/grabber.py
+++ robot/hardware/grabber.py
@@ -9,16 +9,17 @@ from robot.hardware.linear_actuator import LinearActuator
 class Grabber:
     """Class for cleanly controlling grabber subsystem"""
 
-    cup_distance = 0
-    dispense_pwm = 0
-    dispense_time = 0
+    cup_distance: int = 0
+    cup_extension_time: float = 0.0
+    dispense_pwm: int = 0
+    dispense_time: float = 0.0
 
     def __init__(
             self,
             pi: Type[pigpio.pi],
-            linear_actuator_pwm_pin,
-            brushed_motor_pwm_pin,
-            brushed_motor_direction_pin):
+            linear_actuator_pwm_pin: int,
+            brushed_motor_pwm_pin: int,
+            brushed_motor_direction_pin: int) -> None:
         """Initialize linear actuator and brushed motor objects"""
         self.motor = BrushedMotor(
             pi,
@@ -35,18 +36,19 @@ class Grabber:
         """Extend grabbing mechanism and grab beads"""
         pass
 
-    def retract(self):
+    def retract(self) -> None:
         """Retract grabbing mechanism"""
         self.actuator.set_extension_minimum()
 
-    def extend_to_cup(self):
+    def extend_to_cup(self) -> None:
         """Extend grabbing mechanism to hover over cup"""
         self.actuator.set_extension_percent(self.cup_distance)
+        time.sleep(self.cup_extension_time)
 
-    def dispense_beads(self):
+    def dispense_beads(self) -> None:
         """Rotate grabbing mechanism to drop beads"""
         self.motor.set_direction_backward()
-        self.motor.set_motor_pwm(self.dispense_beads)
+        self.motor.set_motor_pwm(self.dispense_pwm)
         time.sleep(self.dispense_time)
         self.motor.set_motor_pwm(0)
         self.motor.set_direction_forward()
